2618cd9772fe338f83b9a5157c95abc8dff6ed01
namikazext-bit <namikazext@gmail.com>
2026-05-01 15:25:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
 const P = require('pino')
+const qrcode = require('qrcode-terminal')
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState('session')
 
   const sock = makeWASocket({
     logger: P({ level: 'silent' }),
-    auth: state,
-    printQRInTerminal: true
+    auth: state
   })
 
   sock.ev.on('creds.update', saveCreds)
 
   sock.ev.on('connection.update', (update) => {
-    const { connection } = update
+    const { connection, qr } = update
+
+    if (qr) {
+      console.log('📱 ESCANEIE O QR ABAIXO:')
+      qrcode.generate(qr, { small: true })
+    }
 
     if (connection === 'open') {
       console.log('✅ BOT CONECTADO')
